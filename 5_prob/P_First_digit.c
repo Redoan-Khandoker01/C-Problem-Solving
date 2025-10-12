d1cb525b05485db5fa3b34a9684579e05fd7bde3
Redoan Khandoer <redoankhandoker8066@gmail.com>
2025-10-12 17:34:32 +0600

    First digit

---
+++ 5_prob/P_First_digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+
+   int A;
+   scanf("%d",&A);
+    if(A /1000 %2 ==0)
+    {
+        printf("EVEN");
+    }
+    else{
+        printf("ODD");
+    }
+  
+    return 0;
+}
